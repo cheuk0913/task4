51a0e3f288cf2066629e5bbf59b8fb3a05744477
cheuk0913 <115674591+cheuk0913@users.noreply.github.com>
2022-10-27 09:25:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,27 @@
 let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
-    x = 0
-    while (x <= 4) {
-        led.plot(x, 0)
-        basic.pause(200)
-        basic.clearScreen()
-        x += 1
-    }
+	
 })
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        led.plot(x, 0)
-        basic.pause(200)
         basic.clearScreen()
+        led.plot(x, 0)
         x += 1
+        basic.pause(200)
     }
     if (x > 4) {
-        x += 0
+        x = 0
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        led.plot(0, y)
+        y += 1
+        basic.pause(200)
+    }
+    if (y > 4) {
+        y = 0
     }
 })
